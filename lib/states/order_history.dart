f0bc5cd9a0f7029bf32d0f12c3fcd39dd61b0ae2
ung <test@abc.com>
2022-04-12 04:56:53 +0700

    update

--- lib/states/order_history.dart
+++ lib/states/order_history.dart
@@ -1,5 +1,11 @@
+// ignore_for_file: avoid_print
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:frongeasyshop/utility/my_constant.dart';
+import 'package:frongeasyshop/widgets/show_process.dart';
+import 'package:frongeasyshop/widgets/show_text.dart';
 
 class OrderHistory extends StatefulWidget {
   const OrderHistory({Key? key}) : super(key: key);
@@ -9,6 +15,33 @@ class OrderHistory extends StatefulWidget {
 }
 
 class _OrderHistoryState extends State<OrderHistory> {
+  var user = FirebaseAuth.instance.currentUser;
+  bool load = true;
+  bool? haveData;
+
+  @override
+  void initState() {
+    super.initState();
+    readOrder();
+  }
+
+  Future<void> readOrder() async {
+    await FirebaseFirestore.instance
+        .collection('order')
+        .where('uidShopper', isEqualTo: user!.uid)
+        .get()
+        .then((value) {
+      print('value ==> ${value.docs}');
+      if (value.docs.isEmpty) {
+        haveData = false;
+      } else {
+        haveData = true;
+      }
+      load = false;
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +49,16 @@ class _OrderHistoryState extends State<OrderHistory> {
         backgroundColor: MyConstant.primart,
         title: const Text('ประวัติรายการสั่งซื้อ'),
       ),
+      body: load
+          ? const ShowProcess()
+          : haveData!
+              ? Text('user ==> ${user!.uid}')
+              : Center(
+                  child: ShowText(
+                    title: 'No Order',
+                    textStyle: MyConstant().h1Style(),
+                  ),
+                ),
     );
   }
 }
